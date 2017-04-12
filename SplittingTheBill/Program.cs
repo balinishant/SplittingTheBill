0ceffda1524c7b9880ed7c221a3be41bd87eed3c
Nishant Bali <bali.nishant1@gmail.com>
2017-04-12 10:32:30 -0400

    Updated 12th April 2017
    Splitting The Bill

---
+++ SplittingTheBill/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SplittingTheBill
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+                String FilePathRead = Console.ReadLine();
+                ReadFromFile(FilePathRead);
+        }
+
+        public static void ReadFromFile(String filePath)
+        {
+
+            StreamWriter sw = File.CreateText(filePath+".out");
+            FileStream reader = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(reader);
+
+            String data = "";
+            List<float> list = new List<float>();
+
+
+            while ((data = sr.ReadLine()) != null)
+            {
+                list.Add(float.Parse(data));
+            }
+
+            float[] arr = list.ToArray();
+            int num = 0;
+            float x = arr.Length;
+
+            while (num < (arr.Length - 1))
+            {
+
+                float trips = arr[num];
+                float b = 0;
+                List<float> list2 = new List<float>();
+                List<float> list3 = new List<float>();
+                
+                for (int j = 0; j < trips; j++)
+                {
+                    num++;
+                    float purchases = arr[num];
+                    float a = 0;
+
+                    for (int k = 0; k < purchases; k++)
+                    {
+                        list2.Add(arr[num + 1]);
+                        a = a + arr[num + 1];
+                        num++;
+
+                    }
+                    
+                    b += a;
+                    list3.Add(a);
+                }
+                float avg = b / trips;
+                for (int l = 0; l <list3.Count(); l++)
+                {
+                    if (avg<list3[l])
+                    {
+                        float m = list3[l]-avg;
+                        sw.WriteLine("("+Math.Round(m,2)+")");
+                    }
+                    else if (avg > list3[l])
+                    {
+                        float m = avg - list3[l];
+                        sw.WriteLine(Math.Round(m, 2));
+                    }
+                }
+                sw.WriteLine("");
+
+                list2.Clear();
+
+                num++;
+
+            }
+
+            sw.Flush();
+        }
+    }
+    
+}
